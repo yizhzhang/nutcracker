5227fe5cb383afbec4cc2586663abddc3a252da4
Louis Zhang <cool.yzhang@gmail.com>
2018-12-20 17:10:14 -0800

    initial commit

---
+++ src/main/java/com/cyclone/nutcracker/NutcrackerApplication.java
@@ -0,0 +1,14 @@
+package com.cyclone.nutcracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutcrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutcrackerApplication.class, args);
+	}
+
+}
+
